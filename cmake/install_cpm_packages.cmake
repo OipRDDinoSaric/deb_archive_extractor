22c6936f597f071910df6e1cfafb68f72b7ddc88
OipRDDinoSaric <account@oip-rd.com>
2024-08-19 17:15:43 +0200

    Suppress warning from third party libraries

--- cmake/install_cpm_packages.cmake
+++ cmake/install_cpm_packages.cmake
@@ -1,3 +1,5 @@
+include(${CMAKE_CURRENT_LIST_DIR}/utility.cmake)
+
 CPMAddPackage(
         NAME fmt
         GITHUB_REPOSITORY fmtlib/fmt
@@ -8,6 +10,18 @@ CPMAddPackage(
         NAME Boost
         VERSION 1.86.0
         GITHUB_REPOSITORY boostorg/boost
-        OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES ;iostreams" # Note the escapes!
+        # Included libraries separators must be escaped, e.g., iostreams\\\;asio
+        OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES iostreams"
         GIT_TAG "boost-1.86.0"
 )
+
+if (false)
+    CPMAddPackage(
+            NAME zlib
+            VERSION 1.3.1
+            GITHUB_REPOSITORY madler/zlib
+    )
+endif ()
+
+declare_system_library(Boost::iostreams)
+declare_system_library(fmt::fmt)
