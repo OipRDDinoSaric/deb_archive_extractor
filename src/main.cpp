d694186f6f5d3aab9a2624f0cb5c2aed2823ae36
OipRDDinoSaric <account@oip-rd.com>
2024-08-21 13:37:20 +0200

    Add PoC test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
-#include <fmt/core.h>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
 
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
 
+#include <boost/iostreams/device/file.hpp>
 
-int
-main()
-{
-    fmt::print("Dino.");
+int main() {
+    std::filesystem::path inputFilePath{
+            "./text.txt.gz"};
+    std::filesystem::path outputFilePath{
+            "./text2.txt"};
+
+    std::ifstream inputFileStream(inputFilePath,
+                                  std::ios_base::binary);
+    boost::iostreams::filtering_istream decompressingStream;
+
+    decompressingStream.push(boost::iostreams::gzip_decompressor());
+    decompressingStream.push(inputFileStream);
+
+    std::ofstream outputFileStream{outputFilePath,
+                                   std::ios_base::binary};
+
+    outputFileStream << decompressingStream.rdbuf();
+
     return 0;
 }
